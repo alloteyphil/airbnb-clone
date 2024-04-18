7b70335c416cc80380cadb9e4dc6e8086907d91e
“Philip <alloteyphilip@gmail.com>
2024-04-18 22:21:55 +0000

    Implemented booking and stay card details

--- app/stays/[id]/page.js
+++ app/stays/[id]/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { getSingleStay } from "@/lib/actions/getSingularStay.action";
 import { CalendarClockIcon, DoorOpenIcon, ShareIcon } from "lucide-react";
 import HeartIcon from "@/app/components/HeartIcon";
@@ -165,8 +166,32 @@ const page = async ({ params: { id } }) => {
           </div>
         </div>
 
-        <div className="w-1/3 pt-2 pl-10">
+        <div className="w-1/3 pt-2 pl-10 flex flex-col gap-8">
           <Booking price={price} />
+          <div className="border rounded-2xl px-4 py-6 gap-4 flex items-center">
+            <svg
+              viewBox="0 0 48 48"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              role="presentation"
+              focusable="false"
+              className="h-[32px] w-[32px] fill-[#E31C3D] stroke-[#E31C3D]"
+            >
+              <g stroke="none">
+                <path
+                  d="m32.62 6 9.526 11.114-18.146 23.921-18.147-23.921 9.526-11.114z"
+                  fillOpacity=".2"
+                ></path>
+                <path d="m34.4599349 2 12.8243129 14.9616983-23.2842478 30.6928721-23.28424779-30.6928721 12.82431289-14.9616983zm-17.9171827 16h-12.52799999l18.25899999 24.069zm27.441 0h-12.528l-5.73 24.069zm-14.583 0h-10.802l5.4012478 22.684zm-15.92-12.86-9.30799999 10.86h11.89399999zm19.253-1.141h-17.468l2.857 12.001h11.754zm1.784 1.141-2.586 10.86h11.894z"></path>
+              </g>
+            </svg>
+            <div className="flex flex-col gap-0.5">
+              <p className="font-normal">This is a rare find</p>
+              <p className="text-gray-500 text-sm">
+                {host}'s place is usually fully booked.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
