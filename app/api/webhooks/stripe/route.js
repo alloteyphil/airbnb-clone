d0f44942f499084b9904fa5b4da1520b2b55bbd0
Philip Allotey <philipallotey@Philips-MacBook-Pro.local>
2024-06-08 23:04:43 +0000

    Added small fixes

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -1,10 +1,14 @@
-import stripe from "stripe";
+import Stripe from "stripe";
 import { NextResponse } from "next/server";
 import { getSingleStay } from "@/lib/actions/getSingularStay.action";
 import { getUserByClerk } from "@/lib/actions/user.actions";
 import { createBooking } from "@/lib/actions/booking.actions";
 
 export default async function POST(req) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-04-10",
+  });
+
   const body = await req.text();
   const sig = req.headers.get("stripe-signature");
   const endpointSecret = process.env.STRIPE_WEBHOOK_SIGNING_SECRET_KEY;
