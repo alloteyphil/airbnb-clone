54cbd885dbb40a1925fcc2236b68a70474803784
Philip Allotey <philipallotey@Philips-MacBook-Pro.local>
2024-06-09 06:53:15 +0000

    Added small fixes

--- app/api/webhooks/stripe/route.js
+++ app/api/webhooks/stripe/route.js
@@ -7,7 +7,7 @@ import { headers } from "next/headers";
 
 export async function POST(req) {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: "2023-10-16",
+    apiVersion: "2024-04-10",
   });
 
   const body = await req.text();
